5e9e8069f1e4c8e3508ad70658782d645d21bb67
victorcobden <vhcobden@gmail.com>
2017-06-12 22:34:19 -0500

    Init

--- DhamycMaps/DhamycMaps.Android/MainActivity.cs
+++ DhamycMaps/DhamycMaps.Android/MainActivity.cs
@@ -20,6 +20,8 @@ namespace DhamycMaps.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Xamarin.FormsMaps.Init(this, bundle);
+
             LoadApplication(new App());
         }
     }
--- DhamycMaps/DhamycMaps/MainPage.xaml.cs
+++ DhamycMaps/DhamycMaps/MainPage.xaml.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using Plugin.Geolocator;
+using Xamarin.Forms.Maps;
+
 namespace DhamycMaps
 {
     public partial class MainPage : ContentPage
@@ -12,6 +15,59 @@ namespace DhamycMaps
         public MainPage()
         {
             InitializeComponent();
+            
+        }
+
+        private async void GetLocationButton_Clicked(object sender, EventArgs e)
+        {
+            isBusy.IsRunning = true;
+            isBusy.IsVisible = true;
+            try
+            {
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+                var position = await locator.GetPositionAsync(10000);
+                //var map = new Map(
+                //             MapSpan.FromCenterAndRadius(
+                //               new Position(position.Latitude, position.Longitude),
+                //               Distance.FromMiles(0.3)
+                //             )
+                //)
+                //{
+                //    IsShowingUser = true,
+                //    HeightRequest = 100,
+                //    WidthRequest = 960,
+                //    VerticalOptions = LayoutOptions.FillAndExpand
+                //};
+                //var positionPin = new Position(position.Latitude, position.Longitude);
+                //var pin = new Pin
+                //{
+                //    Type = PinType.Place,
+                //    Position = positionPin,
+                //    Label = "Estas Aquí",
+                //    Address = "CORREEEEE!!!"
+                //};
+                //map.Pins.Add(pin);
+
+                //isBusy.IsRunning = false;
+                //isBusy.IsVisible = false;
+
+                var stack = new StackLayout { Spacing = 0, Margin=20 };
+
+                stack.Children.Add(new Label { Text = "Dhamyc Community!", TextColor = Color.Accent, HorizontalTextAlignment=TextAlignment.Center, FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)), FontAttributes = FontAttributes.Bold });
+                stack.Children.Add(new Label { Text = $"Longitud: {position.Latitude.ToString()}" , HorizontalTextAlignment=TextAlignment.Center});
+                stack.Children.Add(new Label { Text = $"Longitud: {position.Longitude.ToString()}", HorizontalTextAlignment = TextAlignment.Center });
+                //stack.Children.Add(map);
+                //Content = stack;
+
+            }
+            catch (Exception ex)
+            {
+                string msge = ex.Message;
+            }
+            
+
+            
         }
     }
 }
